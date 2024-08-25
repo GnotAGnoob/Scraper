059a79d858a2fd9eed5ee0c4afdd4f53c8f1f08f
GnotAGnoob <danisvatek@gmail.com>
2024-08-25 21:48:45 +0200

    working table

--- cmd/console/main.go
+++ cmd/console/main.go
@@ -12,6 +12,13 @@ import (
 	"github.com/jedib0t/go-pretty/v6/table"
 )
 
+func getDisplayText(text string, err error) string {
+	if err != nil {
+		return "error"
+	}
+	return text
+}
+
 // todo proper user input
 // todo proper info output
 // todo think about error handling
@@ -48,29 +55,74 @@ func main() {
 			log.Fatal().Err(err).Msg("error while getting products")
 		}
 
-		tab := NewTable()
+		tab := NewTable(len(products))
 
+		fmt.Println("Found products:")
 		for _, product := range products {
+			if product == nil || product.Value == nil || product.ScrapeErr != nil {
+				log.Error().Err(product.ScrapeErr).Msg("error while getting product")
+				continue
+			}
+
 			log.Debug().Msgf("Product: %+v", product.Value)
 			log.Debug().Err(product.ScrapeErr)
 			log.Debug().Msgf("Nutritions: %+v", product.Value.Nutrition.Value)
 			log.Debug().Err(product.Value.Nutrition.ScrapeErr)
 			log.Debug().Msg("\n")
 
+			name := getDisplayText(product.Value.Name.Value, product.Value.Name.ScrapeErr)
+			price := getDisplayText(product.Value.Price.Value, product.Value.Price.ScrapeErr)
+			pricePerKg := getDisplayText(product.Value.PricePerKg.Value, product.Value.PricePerKg.ScrapeErr)
+			unit := getDisplayText(product.Value.Unit.Value, product.Value.Unit.ScrapeErr)
+
+			soldOutText := "available"
+			if product.Value.IsSoldOut {
+				soldOutText = "sold out"
+			}
+
+			nutrition := product.Value.Nutrition
+			nutritionErr := "none" // i would like to use longer text but the fucking table uses width of the first merge column instead of the whole space
+			if nutrition.ScrapeErr != nil {
+				nutritionErr = nutrition.ScrapeErr.Error()
+			}
+
+			rowConfig := table.RowConfig{AutoMerge: true}
+			calories := nutritionErr
+			protein := nutritionErr
+			fat := nutritionErr
+			saturatedFat := nutritionErr
+			carbs := nutritionErr
+			sugar := nutritionErr
+			fiber := nutritionErr
+			ingredients := nutritionErr
+
+			if nutrition.ScrapeErr == nil && nutrition.Value != nil {
+				rowConfig = table.RowConfig{AutoMerge: false}
+				calories = getDisplayText(nutrition.Value.Calories.Value, nutrition.Value.Calories.ScrapeErr)
+				protein = getDisplayText(nutrition.Value.Protein.Value, nutrition.Value.Protein.ScrapeErr)
+				fat = getDisplayText(nutrition.Value.Fat.Value, nutrition.Value.Fat.ScrapeErr)
+				saturatedFat = getDisplayText(nutrition.Value.SaturatedFat.Value, nutrition.Value.SaturatedFat.ScrapeErr)
+				carbs = getDisplayText(nutrition.Value.Carbs.Value, nutrition.Value.Carbs.ScrapeErr)
+				sugar = getDisplayText(nutrition.Value.Sugar.Value, nutrition.Value.Sugar.ScrapeErr)
+				fiber = getDisplayText(nutrition.Value.Fiber.Value, nutrition.Value.Fiber.ScrapeErr)
+				ingredients = getDisplayText(nutrition.Value.Ingredients.Value, nutrition.Value.Ingredients.ScrapeErr)
+			}
+
 			tab.AppendRow(table.Row{
-				product.Value.Name.Value,
-				product.Value.Price.Value,
-				product.Value.PricePerKg.Value,
-				product.Value.Unit.Value,
-				product.Value.Nutrition.Value.Calories.Value,
-				product.Value.Nutrition.Value.Protein.Value,
-				product.Value.Nutrition.Value.Fat.Value,
-				product.Value.Nutrition.Value.SaturatedFat.Value,
-				product.Value.Nutrition.Value.Carbs.Value,
-				product.Value.Nutrition.Value.Sugar.Value,
-				product.Value.Nutrition.Value.Fiber.Value,
-				product.Value.Nutrition.Value.Ingredients.Value,
-			})
+				name,
+				soldOutText,
+				price,
+				pricePerKg,
+				unit,
+				calories,
+				protein,
+				fat,
+				saturatedFat,
+				carbs,
+				sugar,
+				fiber,
+				ingredients,
+			}, rowConfig)
 		}
 
 		tab.Render()
--- cmd/console/table.go
+++ cmd/console/table.go
@@ -5,18 +5,19 @@ import (
 	"os"
 	"reflect"
 
-	"github.com/jedib0t/go-pretty/text"
 	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/jedib0t/go-pretty/v6/text"
 	"golang.org/x/term"
 )
 
 type headerCategory struct {
 	Name        string
-	WidthWeight int
+	WidthWeight float64
 }
 
 type header struct {
 	Product      headerCategory
+	Status       headerCategory
 	Price        headerCategory
 	PricePerKg   headerCategory
 	Unit         headerCategory
@@ -32,16 +33,17 @@ type header struct {
 
 var headerDefinition = header{
 	Product:      headerCategory{Name: "Product", WidthWeight: 3},
+	Status:       headerCategory{Name: "Status", WidthWeight: 1},
 	Price:        headerCategory{Name: "Price", WidthWeight: 1},
-	PricePerKg:   headerCategory{Name: "Price per kg", WidthWeight: 1},
-	Unit:         headerCategory{Name: "Unit", WidthWeight: 1},
+	PricePerKg:   headerCategory{Name: "Price per kg", WidthWeight: 1.75},
+	Unit:         headerCategory{Name: "Unit", WidthWeight: 0.75},
 	Calories:     headerCategory{Name: "Calories", WidthWeight: 1},
-	Protein:      headerCategory{Name: "Protein", WidthWeight: 1},
-	Fat:          headerCategory{Name: "Fat", WidthWeight: 1},
-	SaturatedFat: headerCategory{Name: "Saturated Fat", WidthWeight: 1},
-	Carbs:        headerCategory{Name: "Carbs", WidthWeight: 1},
-	Sugar:        headerCategory{Name: "Sugar", WidthWeight: 1},
-	Fiber:        headerCategory{Name: "Fiber", WidthWeight: 1},
+	Protein:      headerCategory{Name: "Protein", WidthWeight: 0.8},
+	Fat:          headerCategory{Name: "Fat", WidthWeight: 0.75},
+	SaturatedFat: headerCategory{Name: "Saturated Fat", WidthWeight: 0.8},
+	Carbs:        headerCategory{Name: "Carbs", WidthWeight: 0.75},
+	Sugar:        headerCategory{Name: "Sugar", WidthWeight: 0.75},
+	Fiber:        headerCategory{Name: "Fiber", WidthWeight: 0.75},
 	Ingredients:  headerCategory{Name: "Ingredients", WidthWeight: 3},
 }
 
@@ -50,16 +52,16 @@ const MIN_TABLE_WIDTH = 80
 const INDEX_WIDTH = 5 // 3 for the index + 2 for the borders of index
 const ITEM_WIDTH = 3  // 3 because padding on each side (2) + 1 border
 
-func NewTable() table.Writer {
+func NewTable(itemsCount int) table.Writer {
 	tab := table.NewWriter()
 	tab.SetAutoIndex(true)
 	tab.SetOutputMirror(os.Stdout)
 
-	sumWidthWeight := 0
+	var sumWidthWeight float64
 	v := reflect.ValueOf(headerDefinition)
 	for i := 0; i < v.NumField(); i++ {
 		field := v.Field(i)
-		sumWidthWeight += int(field.FieldByName("WidthWeight").Int())
+		sumWidthWeight += field.FieldByName("WidthWeight").Float()
 	}
 
 	termWidth, _, err := term.GetSize(int(os.Stdout.Fd()))
@@ -67,7 +69,7 @@ func NewTable() table.Writer {
 		termWidth = MIN_TABLE_WIDTH
 	}
 	width := math.Min(float64(termWidth), MAX_TABLE_WIDTH)
-	width -= float64(v.NumField()*ITEM_WIDTH + INDEX_WIDTH)
+	width -= float64(v.NumField()*ITEM_WIDTH + INDEX_WIDTH + itemsCount/10)
 	widthFragment := float64(width) / float64(sumWidthWeight)
 
 	var columnConfigs []table.ColumnConfig
@@ -76,9 +78,9 @@ func NewTable() table.Writer {
 	for i := 0; i < v.NumField(); i++ {
 		field := v.Field(i)
 		fieldName := field.FieldByName("Name").String()
-		fieldWeight := int(field.FieldByName("WidthWeight").Int())
+		fieldWeight := field.FieldByName("WidthWeight").Float()
 
-		widthMax := widthFragment * float64(fieldWeight)
+		widthMax := widthFragment * fieldWeight
 		widthRemainder := math.Mod(widthMax, 1)
 		widthMax -= widthRemainder
 		overflowWidth += widthRemainder
@@ -89,13 +91,10 @@ func NewTable() table.Writer {
 		}
 
 		config := table.ColumnConfig{
-			Name:     fieldName,
-			WidthMax: int(widthMax),
-			WidthMin: int(widthMax),
-		}
-
-		if fieldName == "Product" || fieldName == "Ingredients" {
-			config.WidthMaxEnforcer = text.Trim
+			Name:             fieldName,
+			WidthMax:         int(widthMax),
+			WidthMin:         int(widthMax),
+			WidthMaxEnforcer: text.Trim,
 		}
 
 		columnConfigs = append(columnConfigs, config)
