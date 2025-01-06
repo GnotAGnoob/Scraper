dfa7e95df47a1e8df4844a9950924f40f5a3584d
GnotAGnoob <danisvatek@gmail.com>
2025-01-06 13:37:52 +0100

    wider progress bar

--- cmd/console/progressBar.go
+++ cmd/console/progressBar.go
@@ -2,6 +2,9 @@ package main
 
 import "github.com/schollz/progressbar/v3"
 
+const termWidthTextSubtraction = 30
+
 func getProgressBar(text string) *progressbar.ProgressBar {
-	return progressbar.NewOptions(100, progressbar.OptionSetRenderBlankState(true), progressbar.OptionClearOnFinish(), progressbar.OptionSetDescription(text), progressbar.OptionShowCount(), progressbar.OptionSetElapsedTime(true), progressbar.OptionSetPredictTime(true))
+	termSubstract := termWidthTextSubtraction + len(text)
+	return progressbar.NewOptions(100, progressbar.OptionSetRenderBlankState(true), progressbar.OptionClearOnFinish(), progressbar.OptionSetDescription(text), progressbar.OptionShowCount(), progressbar.OptionSetElapsedTime(true), progressbar.OptionSetPredictTime(true), progressbar.OptionSetWidth(int(getTermWidth())-termSubstract))
 }
